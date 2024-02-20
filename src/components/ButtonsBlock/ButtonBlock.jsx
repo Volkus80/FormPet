e614a9d3133cd9adc7156277f4264a633e3fe513
volkus <volkus80@gmail.com>
2024-02-21 00:05:14 +0200

    second page

--- src/components/ButtonsBlock/ButtonBlock.jsx
+++ src/components/ButtonsBlock/ButtonBlock.jsx
@@ -1,34 +1,40 @@
 import s from "./ButtonBlock.module.scss";
 // import cn from "classnames";
 import bind from "classnames/bind";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { stepDown, stepUp } from "../../store/steps/stepsActions";
+import { stepSelector } from "../../store/steps/stepsSelectors";
 
 const cx = bind.bind(s);
 
 function ButtonBlock() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const step = useSelector(stepSelector);
 
-    const forwardButtonStyles = cx({
-        button: true,
-        button_forward: true
-    });
+  const forwardButtonStyles = cx({
+    button: true,
+    button_forward: true,
+    button_forward_confirm: step === 4,
+  });
 
-    const backButtonStyle = cx({
-        button: true,
-        button_back: true
-    });
+  const backButtonStyle = cx({
+    button: true,
+    button_back: true,
+    button_back_hide: step === 1,
+  });
 
-    return (
-        <div className={s.buttons}>
-            <button className={forwardButtonStyles} onClick={() => dispatch(stepUp)}>
-                Next Step
-            </button>
-            <button className={backButtonStyle} onClick={() => dispatch(stepDown)}>
-                Go Back
-            </button>
-        </div>
-    );
+  let forwardButtonText = step < 4 ? "Next Step" : "Confirm";
+
+  return (
+    <div className={s.buttons}>
+      <button className={forwardButtonStyles} onClick={() => dispatch(stepUp)}>
+        {forwardButtonText}
+      </button>
+      <button className={backButtonStyle} onClick={() => dispatch(stepDown)}>
+        Go Back
+      </button>
+    </div>
+  );
 }
 
 export { ButtonBlock };
--- src/components/MainPanel/MainPanel.jsx
+++ src/components/MainPanel/MainPanel.jsx
@@ -5,16 +5,19 @@ import { MainPanelBody } from "../MainPanelBody/MainPanelBody";
 import cn from "classnames";
 import bind from "classnames/bind";
 import s from "./MainPanel.module.scss";
+import { UseSelector, useSelector } from "react-redux";
+import { stepSelector } from "../../store/steps/stepsSelectors";
 
 const cx = bind.bind(s);
-function MainPanel({ active = 1 }) {
+function MainPanel() {
+  const step = useSelector(stepSelector);
   return (
     <div className={s.panel}>
       <div className={s.panel_header}>
-        <p className={s.panel_header_head}>{panelText[active].header}</p>
-        <p className={s.panel_header_description}>{panelText[active].text}</p>
+        <p className={s.panel_header_head}>{panelText[step].header}</p>
+        <p className={s.panel_header_description}>{panelText[step].text}</p>
       </div>
-      <MainPanelBody active={active} />
+      <MainPanelBody step={step} />
       <ButtonBlock />
     </div>
   );
